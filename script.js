0f82a7a271fa0d0bb9d609761ef4316dfd3a26d7
JonLedo <jonledo.code@gmail.com>
2022-08-22 17:29:31 -0400

    added js, used Date object, and transform with transform origin to move hands of clock, some missing css styles

---
+++ script.js
@@ -0,0 +1,36 @@
+const secondsHand = getElement('#seconds-hand')
+const minutesHand = getElement('#minutes-hand')
+const hoursHand = getElement('#hours-hand')
+
+
+function getTime() {
+  const currentTime = new Date()
+  const seconds = currentTime.getSeconds()
+  const minutes = currentTime.getMinutes()
+  const hours = currentTime.getHours()
+
+  // 360degrees in a circle, 60s in an hour. 360/60 = 6
+  const timeInterval = 6
+
+  secondsHand.style.transform = `rotate(${(seconds * timeInterval)}deg)`
+  secondsHand.style.transformOrigin = '0 100%'
+
+  minutesHand.style.transform = `rotate(${(minutes * timeInterval)}deg)`
+  minutesHand.style.transformOrigin = '0 100%'
+
+  hoursHand.style.transform = `rotate(${(hours * timeInterval)}deg)`
+  hoursHand.style.transformOrigin = '0 100%'
+}
+
+setInterval(getTime, 100)
+
+
+// query Selector function
+function getElement (selection) {
+  const element = document.querySelector(selection)
+  if (element) {
+    return element
+  }
+  throw new Error (`Please check "selection" selector, no such element exists`)
+}
+
